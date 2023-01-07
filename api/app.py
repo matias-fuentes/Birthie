4941de61ce4cc55a4729364fa3825b92bc5ccb57
Matías Fuentes <mfuentesdeveloper@gmail.com>
2023-01-07 15:57:55 -0300

    Removed app.run() line of code

--- api/app.py
+++ api/app.py
@@ -22,9 +22,6 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key=os.environ.get('SECRET_KEY')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
